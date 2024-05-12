5b8a215aa9a8507cd941c9b0686374ab6e9517fa
Kremtastic <kristianetakvam@gmail.com>
2024-05-12 16:06:20 +0200

    Add destroy pipes function
    Also a lot of files seems to be added together in this commit that should have been in earlier.

--- scripts/pipe.gd
+++ scripts/pipe.gd
@@ -5,19 +5,13 @@ signal score_increase
 
 # Destroy pipe when exit screen
 func _on_visible_on_screen_notifier_2d_screen_exited():
-	#print("Pipe destroyed!")
-	#queue_free()
+	print("Pipe destroyed!")
+	queue_free()
 	pass
 
 # Player hits pipe
 func _on_body_entered(body):
 	emit_signal("player_hit_pipe")
-	
-
-
-	
-
 
 func _on_score_count_area_body_entered(body):
-	print("Score!")
 	emit_signal("score_increase")
