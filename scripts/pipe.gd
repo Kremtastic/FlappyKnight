b766c6690fb9e2e71727998642fa638f0f64bb87
Kristian Takvam <kristianetakvam@gmail.com>
2024-05-13 22:42:55 +0200

    Disable destroy pipes func
    Caused performance issues (ironic).

--- scripts/pipe.gd
+++ scripts/pipe.gd
@@ -4,9 +4,9 @@ signal player_hit_pipe
 signal score_increase
 
 # Destroy pipe when exit screen
-func _on_visible_on_screen_notifier_2d_screen_exited():
-	print("Pipe destroyed!")
-	queue_free()
+func _on_visible_on_screen_notifier_2d_screen_exited(): # Lags the game a lot, disabled for now.
+	#print("Pipe destroyed!")
+	#queue_free()
 	pass
 
 # Player hits pipe
